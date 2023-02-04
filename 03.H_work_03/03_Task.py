97e1139b762ab29250305411baf3c973fbff76dc
Lupin <skorpion_94@list.ru>
2023-02-04 16:35:04 +0100

    Задачка список вещественных чисел и прога которая найдет разницу между мин и макс

--- 03.H_work_03/03_Task.py
+++ 03.H_work_03/03_Task.py
@@ -1,16 +1,28 @@
-# Задайте список из вещественных чисел. Напишите программу, 
+# Задайте список из вещественных чисел. Напишите программу,
 # которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
 
 # Пример:
 
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
-import random
-def drob(num):
-    return round(num%1,2)
+# import random
+# def drob(num):
+#     return round(num%1,2)
 
-a = [round(random.uniform(0,10),2) for i in range(random.randint(5,8))]
+# a = [round(random.uniform(0,10),2) for i in range(random.randint(5,8))]
+# print(a)
+# list_drob = list(map(drob,a))
+# print(list_drob)
+# print(max(list_drob), min(list_drob))
+# print(max(list_drob) - min(list_drob))
+# ___________________________________________________________________________
+import random
+a = [round(random.uniform(0, 10), 2) for i in range(random.randint(5, 8))]
 print(a)
-list_drob = list(map(drob,a))
+list_drob = []
+for i in a:
+    if i % 1 != 0:
+        list_drob.append(round(i%1,2))
+
 print(list_drob)
 print(max(list_drob), min(list_drob))
 print(max(list_drob) - min(list_drob))
