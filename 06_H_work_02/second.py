f9f49230e180880bf3d808c12d0bb667ad43e870
Lupin <skorpion_94@list.ru>
2023-02-16 14:53:49 +0100

    Задачка найти сум чисел стоящих на нечетной позиции (с функциями )

--- 06_H_work_02/second.py
+++ 06_H_work_02/second.py
@@ -3,4 +3,6 @@ import random
 a = [random.randint(1,10) for i in range(6)]
 print(a)
 res = sum([value for indx,value in enumerate(a) if indx%2==1])
+# res = list(map(lambda x: a[x] if (x)%2==1 else None,list(range(len(a)))))
+# res = sum([i for i in res if i!=None])
 print(res)
